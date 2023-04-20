c069a8829aab5ca7918bba4a3ab915b7b56dde2e
Martin Cutillo-Rigby <70958005+mcrigby@users.noreply.github.com>
2023-04-20 16:05:18 +0100

    add samples and datasheet

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# hmc5883l
+3-Axis Digital Compass IC
+HMC5883L
+
+var _hmc5883l = HMC5883L.Create();
+
+_hmc5883l.MeasurementsAveraged = MeasurementsAveraged._4;
+_hmc5883l.DataOutputRate = DataOutput._15000mHz;
+_hmc5883l.MeasurementMode = MeasurementMode.Normal;
+_hmc5883l.Gain = Gain._5;
+
+while (true)
+{
+    output($"{_hmc5883l.Heading.Degrees:n2}°");
+    await Task.Delay(500);
+}
+
+KNOWN ISSUE: Contnuous Mode not working.
---
+++ samples/Program.cs
@@ -0,0 +1,27 @@
+﻿using CutilloRigby.Device.HMC5883L;
+
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (s, e) => cts.Cancel();
+
+var _hmc5883l = HMC5883L.Create();
+
+// if (!_hmc5883l.SelfTest(Gain._5, MeasurementMode.Positive_Bias))
+//     Console.WriteLine("Positive Self Test Failed");
+// else
+//     Console.WriteLine($"Positive Bias Passed at Gain {_hmc5883l.Gain}.");
+
+// if (!_hmc5883l.SelfTest(Gain._5, MeasurementMode.Negative_Bias))
+//     Console.WriteLine("Negative Self Test Failed");
+// else
+//     Console.WriteLine($"Negative Bias Passed at Gain {_hmc5883l.Gain}.");
+
+_hmc5883l.MeasurementsAveraged = MeasurementsAveraged._4;
+_hmc5883l.DataOutputRate = DataOutput._15000mHz;
+_hmc5883l.MeasurementMode = MeasurementMode.Normal;
+_hmc5883l.Gain = Gain._5;
+
+while(!cts.IsCancellationRequested)
+{
+    Console.WriteLine($"{_hmc5883l.Heading.Degrees:n2}°");
+    await Task.Delay(500);
+}
